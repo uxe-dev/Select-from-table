135d221c9ecea11027916bb3d2bc6e9b09fa0cb6
uxe-dev <uxe77655@gmail.com>
2024-12-24 10:50:17 +0300

    Add files via upload
    In this simple project I created a variable that has more than one value. At the beginning, the user will choose a number from zero to two and will print the result, which is a name.
    
    I hope that this simple code will benefit anyone.
    
    I allow anyone to use it and modify it.

---
+++ أعطني الإسم من الجدول.py
@@ -0,0 +1,10 @@
+names = "same" , "jake" , "ali"
+number_name=input("shouse 0 to 2: ")
+if number_name == '0':
+    print(names[0])
+elif number_name == '1':
+    print(names[1])
+elif number_name == '2':
+    print(names[2])
+else:
+    print("error")
